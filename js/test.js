4421abd3dcc808e27763f058641a9d5ce6a1b4b5
Oksana999 <oksanam999@gmail.com>
2020-07-03 15:21:59 -0500

    Fix issues with counting score for the test.

--- js/test.js
+++ js/test.js
@@ -3,19 +3,24 @@ function checkTest() {
 
     let answer = form.elements.answer;
     let answer1 = form.elements.answer1;
+    let answer2 = form.elements.answer_2;
 
-    let answers = [answer.value, answer1.value];
+    let answers = [answer.value, answer1.value, answer2.value];
 
-    let correctAnswers = ["It is a template for create an object", "It is a method that belongs to the class"];
+
+    let correctAnswers = ["It is a template for create an object",
+        "It is a method that belongs to the class",
+        "A constructor initializes an object when it is created"];
 
     let myScore = 0;
     for (let i = 0; i < correctAnswers.length; i++) {
         console.log(myScore);
-        if (answers[i] === correctAnswers[i]) {
+        if (answers[i] == correctAnswers[i]) {
             myScore++;
         }
 
     }
 
     document.getElementById("score").innerHTML = "Your score is " + myScore + " out of " + correctAnswers.length;
+    document.getElementById("center1").style = "display:block";
 }
